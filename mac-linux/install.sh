72ec558fbb9fbe43915c54ab658cf160bc45a98b
Abhi Vempati <abhijitvempati@gmail.com>
2021-10-07 14:06:43 -0400

    removed redundant cs install

--- mac-linux/install.sh
+++ mac-linux/install.sh
@@ -1,14 +1,11 @@
 This script installs scala3 to your machine via curl and adding to system paths
 curl -fLo cs https://git.io/coursier-cli-"$(uname | tr LD ld)"
 chmod +x cs
-./cs install cs
 ./cs install cs &> test1.txt
-
-
-tag=$( tail -n 1 text.txt )
+tag=$( tail -n 1 test1.txt )
 eval $tag
 echo "coursier added to system path"
-rm text.txt
+rm test1.txt
 cs update cs
 cs install scala3-compiler
 cs install scala
