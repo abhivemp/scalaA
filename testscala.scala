a7183076912d18c92d812ed93ec2b8feb34dc7d9
Abhi Vempati <abhijitvempati@gmail.com>
2021-10-07 03:16:32 -0400

    Windows 10 script added with testing scala file

---
+++ testscala.scala
@@ -0,0 +1,10 @@
+import scala.collection.mutable.ArrayBuffer
+import scala.io.StdIn.readInt
+
+object example {
+    def main(args: Array[String]) =
+        
+        print("Yay your scala program successfully compiles and runs!!")
+        print("You can ^C now.")
+
+}
---
+++ win/win_install.cmd
@@ -0,0 +1,10 @@
+@REM Windows script to install scala and add3 to path
+bitsadmin /transfer cs-cli https://git.io/coursier-cli-windows-exe "%cd%\cs.exe"
+.\cs --help
+.\cs install.\cs update
+cs install scala3-compiler
+cs install scala
+echo "Testing example program"
+cd ..
+scala3-compiler testscala.scala
+scala testscala
