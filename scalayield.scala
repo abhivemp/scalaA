591d881e343a7550fd600c4b7931eaea5bce5525
Abhi Vempati <abhijitvempati@gmail.com>
2021-10-08 16:20:31 -0400

    Added example code of usage of yield

---
+++ scalayield.scala
@@ -0,0 +1,20 @@
+// Example program that we went over in class that uses the yield keyword
+
+import scala.collection.mutable.ArrayBuffer
+import scala.io.StdIn.readInt
+
+object scalayield {
+    def main(args: Array[String]) = 
+        val sample = ArrayBuffer(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+        var count = 0
+        val retVal = (
+            for
+                i <- sample
+                if i % 2 == 0
+            yield
+                count = count + 1
+        ).to(ArrayBuffer)
+    
+    println(count)
+    println(retVal)
+}
