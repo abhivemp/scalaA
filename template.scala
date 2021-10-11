8213a8088ef1b7ac29c654ae041aec3113e41022
Abhi Vempati <abhijitvempati@gmail.com>
2021-10-11 11:47:40 -0400

    added a template

---
+++ template.scala
@@ -0,0 +1,39 @@
+import scala.io.StdIn.readInt
+import scala.collection.mutable.ArrayBuffer
+
+object matrixManipulation {
+    def main(args: Array[String]) =
+        /**
+        Your code Here
+
+        ****IMPORTANT NOTE*****
+        Indentation Matters    
+
+        ---Section 1---
+        Take in the dimensions for M1 (rows followed by columns)
+        Create and populate ArrayBuffer with user input
+
+        Print M1
+
+        ---Section 2---
+        Take in the dimensions for M2 (rows followed by columns)
+        Create and populate ArrayBuffer with user input
+
+        Print M2
+
+        ---Section 3---
+        Create a new ArrayBuffer that will hold the transpose
+        of M2
+
+        ---Section 4---
+        Create a new ArrayBuffer that will hold the scalar 
+        multiplication of m2 with the user specified multiplier
+
+        ---Section 5---
+        Create a new ArrayBuffer that will hold the addition of 
+        M1 and M2. Matricies must be of equal size in order to add
+
+        
+        */
+
+}
